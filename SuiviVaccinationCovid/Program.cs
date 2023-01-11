c79119d1674cf4d502d7e7156a5eb27cc0b4a41b
RebeccaLapointe <reblapointe@gmail.com>
2023-01-11 18:40:09 -0500

    Code de départ

--- SuiviVaccinationCovid/Program.cs
+++ SuiviVaccinationCovid/Program.cs
@@ -10,7 +10,6 @@ namespace SuiviVaccinationCovid
     // Solution avec délégués
     public class Program
     {
-        public Func<DateTime> FournisseurDeDate { get; set; }
         public VaccinationContext Contexte { get; set; }
 
         public static void Main(string[] _)
@@ -18,12 +17,11 @@ namespace SuiviVaccinationCovid
             // Injection des dépendances
             Program p = new()
             {
-                FournisseurDeDate = () => DateTime.Now,
                 Contexte = new VaccinationContext()
             };
             p.Peupler();
 
-            //p.EnregistrerDose(p.CreerNouvelleDose("PILA95032911", p.Contexte.Vaccins.Single(t => t.Nom == "Pfizer")));
+            p.EnregistrerDose("PILA95032911", p.Contexte.Vaccins.Single(t => t.Nom == "Pfizer"));
 
             Console.WriteLine("La dose la plus récente : ");
             Console.WriteLine(p.LePlusRecent());
@@ -70,23 +68,14 @@ namespace SuiviVaccinationCovid
             }
         }
 
-        public Dose CreerNouvelleDose(string nam, Vaccin vaccin)
+        public void EnregistrerDose(string nam, Vaccin vaccin)
         {
-            return new Dose
+            Dose dose = new()
             {
                 NAMPatient = nam,
                 Vaccin = vaccin,
-                Date = FournisseurDeDate()
+                Date = DateTime.Now
             };
-        }
-
-        public void EnregistrerDose(Dose dose)
-        {
-            var memePatient = Contexte.Doses.Where(d => d.NAMPatient == dose.NAMPatient);
-            if (memePatient.Any() && Math.Abs((dose.Date - memePatient.OrderBy(d => d.Date).Last().Date).Days) < 21)
-                throw new ArgumentException("Les doses doivent être espacées d'au moins 21 jours");
-            if (memePatient.Count() == 1 && memePatient.First().Vaccin != dose.Vaccin)
-                throw new ArgumentException("Les deux premières doses d'un patient doivent être le même type de vaccin");
 
             Contexte.Doses.Add(dose);
             Contexte.SaveChanges();
@@ -94,9 +83,7 @@ namespace SuiviVaccinationCovid
 
         public static Dose LePlusRecent(IQueryable<Dose> doses)
         {
-            if (doses.Any())
-                return doses.OrderBy(d => d.Date).Last();
-            return null;
+            return doses.OrderBy(d => d.Date).Last();
         }
 
         public Dose LePlusRecent()
